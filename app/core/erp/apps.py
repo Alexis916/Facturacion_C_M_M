c88bdf8d3cc94154b0de5540fe202a5b66f0f7d8
unknown <alexis11151@gmail.com>
2023-07-11 15:02:13 -0500

    first commit

---
+++ app/core/erp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class erpConfig(AppConfig):
+    name = 'core.erp'
---
+++ app/core/erp/views/ayuda/view.py
@@ -0,0 +1,4 @@
+from django.views.generic import TemplateView
+
+class ayuda(TemplateView):
+    template_name = 'ayuda.html'
